c0e02c8ab3ac603aa2eae7a04e8bfd5fed581ea1
Daniel Beardsley <daniel.beardsley@gmail.com>
2009-10-16 14:29:28 -0700

    Doh!  forgot to negate!

--- lib/model/active_record/model.rb
+++ lib/model/active_record/model.rb
@@ -44,7 +44,7 @@ module ExtJS
           if options[:exclude]
             self.extjs_record_fields = self.columns.reject {|c| options[:exclude].find {|ex| c.name.to_sym === ex}}.collect {|c| c.name.to_sym}
           elsif options[:only]
-            self.extjs_record_fields = options[:only].reject {|c| self.columns.find {|col| col.name.to_sym === c}}
+            self.extjs_record_fields = options[:only].reject {|c| !self.columns.find {|col| col.name.to_sym === c}}
           end
         elsif !params.empty?
           self.extjs_record_fields = params
