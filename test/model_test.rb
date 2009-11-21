dd85ec6bc72d4ac7052e4a868e1324a0064fddf1
Christopher Scott <christocracy@gmail.com>
2009-11-21 14:41:52 -0500

    Adding more tests

--- test/model_test.rb
+++ test/model_test.rb
@@ -22,8 +22,35 @@ class ModelTest < Test::Unit::TestCase
 	    rec = User.first.to_record
 	    assert rec.kind_of?(Hash) && rec.keys.include?(User.extjs_primary_key)
 	  end
-	  
   end
   
+  context "A User with with Person relationship: User.extjs_fields(:password, :person => [:first, :last])" do
+    setup do
+      User.extjs_fields(:password, :person => [:first, :last])
+	    @fields = User.extjs_record["fields"]
+    end
+    
+    should "User should render a Reader with 4 total fields" do
+	    assert @fields.count === 4
+    end
+    
+    should "Reader fields should contain 'password' field" do
+      assert @fields.find {|f| f[:name] === "password"}
+    end
+    
+    should "Reader fields should contain person_id" do
+      assert @fields.find {|f| f[:name] === "person_id" }
+    end
+    
+    should "Reader fields should contain mapped field 'person.first'" do
+      assert @fields.find {|f| f[:name] === "person_first" and f["mapping"] === "person.first"}
+    end
+    
+    should "Reader fields should contain mapped field 'person.last'" do
+      assert @fields.find {|f| f[:name] === "person_last" and f["mapping"] === "person.last"}
+    end
+    
+  end
+
 end
 
