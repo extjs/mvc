f800c087f98033226d2070b352a5f2283fe95729
Christopher Scott <christocracy@gmail.com>
2009-11-21 17:50:51 -0500

    Implemented data-type test-suite.  DEFAULT currently fails.

--- test/model_test.rb
+++ test/model_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 
 class ModelTest < Test::Unit::TestCase
-  context "Calling extjs_record upon Person and User" do
+  context "Rendering DataReader configuration for Person and User" do
 
 	  setup do
 	    # common stuff for all tests.
@@ -11,6 +11,11 @@ class ModelTest < Test::Unit::TestCase
 	  should "Person should render a Reader config" do
 	    reader = Person.extjs_record
 	    assert reader.kind_of?(Hash) && reader.has_key?("fields") && reader.has_key?("idProperty")
+	  end
+	  
+	  should "Person instance should render with to_record, a Hash containing at least a primary_key" do
+	    rec = Person.first.to_record
+	    assert rec.kind_of?(Hash) && rec.keys.include?(Person.extjs_primary_key)
 	  end
 	  
 	  should "User should render a Reader config" do
@@ -22,6 +27,7 @@ class ModelTest < Test::Unit::TestCase
 	    rec = User.first.to_record
 	    assert rec.kind_of?(Hash) && rec.keys.include?(User.extjs_primary_key)
 	  end
+	  
   end
   
   context "A User with Person relationship: User.extjs_fields(:password, :person => [:first, {:last => {'sortDir' => 'ASC'}}])" do
@@ -53,8 +59,46 @@ class ModelTest < Test::Unit::TestCase
     should "person.last should have additional configuration 'sortDir' => 'ASC'" do
       assert @fields.find {|f| f[:name] === "person_last" and f["sortDir"] === 'ASC' }
     end
+  end
+  
+  context "Fields should render with correct, ExtJS compatible data-types" do
+    setup do
+      @fields = DataType.extjs_record["fields"]
+    end
+    
+    should "Understand 'string'" do
+      assert @fields.find {|f| f[:name] === 'string_column' && f["type"].to_s === 'string'}
+    end
+    should "Understand 'integer' as 'int'" do
+      assert @fields.find {|f| f[:name] === 'integer_column' && f["type"].to_s === 'int'}
+    end
+    should "Understand 'float'" do
+      assert @fields.find {|f| f[:name] === 'float_column' && f["type"].to_s === 'float'}
+    end
+    should "Understand 'decimal' as 'float'" do
+      assert @fields.find {|f| f[:name] === 'decimal_column' && f["type"].to_s === 'float'}
+    end
+    should "Understand 'date'" do
+      assert @fields.find {|f| f[:name] === 'date_column' && f["type"].to_s === 'date'}
+    end
+    should "Understand 'datetime' as 'date'" do
+      assert @fields.find {|f| f[:name] === 'datetime_column' && f["type"].to_s === 'date'}
+    end
+    should "Understand 'time' as 'date'" do
+      assert @fields.find {|f| f[:name] === 'time_column' && f["type"].to_s === 'date'}
+    end
+    should "Understand 'boolean'" do
+      assert @fields.find {|f| f[:name] === 'boolean_column' && f["type"].to_s === 'boolean'}
+    end
+    should "Understand NOT NULL" do
+      assert @fields.find {|f| f[:name] === 'notnull_column' && f["allowBlank"] === false}
+    end
+    should "Understand DEFAULT" do # TODO implement this.
+      assert @fields.find {|f| f[:name] === 'default_column' && f["default"] === true}
+    end
     
   end
-
+  
+  
 end
 
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,6 +29,7 @@ ActiveRecord::Base.establish_connection(config['test'])
 
 ##
 # build User / Person models
+# Move AR-specific stuff to AR test adapter
 #
 class User < ActiveRecord::Base
   include ExtJS::Model
@@ -36,6 +37,10 @@ class User < ActiveRecord::Base
 end
 
 class Person < ActiveRecord::Base
+  include ExtJS::Model
+end
+
+class DataType < ActiveRecord::Base
   include ExtJS::Model
 end
 
@@ -49,14 +54,35 @@ ActiveRecord::Base.connection.create_table :users, :force => true do |table|
   table.column :id, :serial
   table.column :person_id, :integer
   table.column :password, :string
+  table.column :created_at, :date
+  table.column :disabled, :boolean, :default => true
 end
 
 ActiveRecord::Base.connection.create_table :people, :force => true do |table|
   table.column :id, :serial
-  table.column :first, :string
-  table.column :last, :string
-  table.column :email, :integer
+  table.column :first, :string, :null => false
+  table.column :last, :string, :null => false
+  table.column :email, :string, :null => false
 end
+
+##
+# Mock a Model for testing data-types
+#
+ActiveRecord::Base.connection.create_table :data_types, :force => true do |table|
+  table.column :id, :serial
+  table.column :string_column, :string
+  table.column :decimal_column, :decimal
+  table.column :float_column, :float
+  table.column :date_column, :date
+  table.column :datetime_column, :datetime
+  table.column :time_column, :time
+  table.column :email, :string
+  table.column :integer_column, :integer
+  table.column :notnull_column, :string, :null => false
+  table.column :default_column, :boolean, :default => true
+  table.column :boolean_column, :boolean
+end
+
 
 ##
 # create a couple of related instances.
